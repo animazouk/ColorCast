b478faa7e9cdd984562b8d44737f503db5133b8f
Divyansh Bajpai <divyanshbajpai27@gmail.com>
2024-08-03 15:46:30 +0530

    Initialized

---
+++ app/src/main/java/com/example/colorcast/SplashLogo.java
@@ -0,0 +1,40 @@
+package com.example.colorcast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashLogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_logo);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(6000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashLogo.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };thread.start();
+    }
+}
